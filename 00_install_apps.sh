3981becd411caf8807bddb2b12553d72b0a6c1f8
Vitor Mello <vitor.mello@nubank.com.br>
2024-06-18 15:11:49 -0300

    Change to z shell

--- 00_install_apps.sh
+++ 00_install_apps.sh
@@ -1,5 +1,5 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/vho/.bash_profile
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> ~/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
 brew install git
