a6f32701ae790b0d9292c1e4cfaa994cfac7109a
Vitor Mello <vitor.mello@nubank.com.br>
2024-06-18 22:26:29 -0300

    fix typo

--- 02_r.sh
+++ 02_r.sh
@@ -5,5 +5,5 @@ brew install --cask rstudio
 conda create -n r-kernel -y
 conda activate r-kernel
 conda install r-recommended r-irkernel -y
-conda install Jupyter
+conda install Jupyter -y
 R -e 'IRkernel::installspec()'
