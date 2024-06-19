43b50edafadec2acda72f777c90d101e6492b91b
Vitor Mello <vitor.mello@nubank.com.br>
2024-06-18 22:22:16 -0300

    add conda channel

--- 01_python.sh
+++ 01_python.sh
@@ -2,6 +2,9 @@ brew install python
 brew install miniconda
 conda init zsh
 
+conda config --add channels conda-forge
+conda config --set channel_priority strict
+
 echo "alias pip=pip3"
 echo "alias python=python3"
 source ~/.zprofile
