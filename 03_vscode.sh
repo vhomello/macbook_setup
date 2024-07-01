64dc755938829309e674dd886fc094c113d175fc
Vitor Mello <vitor.mello@nubank.com.br>
2024-07-01 13:06:51 -0300

    Create 03_vscode.sh

---
+++ 03_vscode.sh
@@ -0,0 +1,5 @@
+code --install-extension ms-python.python
+code --install-extension eamodio.gitlens
+code --install-extension ms-toolsai.jupyter
+code --install-extension yzhang.markdown-all-in-one
+code --install-extension mechatroner.rainbow-csv
